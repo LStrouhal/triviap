3b34f898bc59099413799081dea8445eac5bf918
Laura Strouhal <laura_strouhal@hotmail.com>
2021-03-25 14:36:42 +0100

    connected frontend to backend

---
+++ frontend/src/components/CategoryList.js
@@ -0,0 +1,101 @@
+export const categoryList = [
+    {
+        value: 1,
+        label: "General Knowledge"
+    },
+    {
+        value: 2,
+        label: "ntertainment: Books"
+    },
+    {
+        value: 3,
+        label: "Entertainment: Film"
+    },
+    {
+        value: 4,
+        label: "Entertainment: Music"
+    },
+    {
+        value: 5,
+        label: "Entertainment: Musicals & Theaters"
+    },
+    {
+        value: 6,
+        label: "Entertainment: Television"
+    },
+    {
+        value: 7,
+        label: "Entertainment: Video Games"
+    },
+    {
+        value: 8,
+        label: "Entertainment: Board Games"
+    },
+    {
+        value: 9,
+        label: "Science & Nature"
+    },
+    {
+        value: 10,
+        label: "Science: Computers"
+    },
+    {
+        value: 11,
+        label: "Science: Mathematics"
+    },
+    {
+        value: 12,
+        label: "Mythology"
+    },
+    {
+        value: 13,
+        label: "Sports"
+    },
+    {
+        value: 14,
+        label: "Geography"
+    },
+    {
+        value: 15,
+        label: "History"
+    },
+    {
+        value: 16,
+        label: "Politics"
+    },
+    {
+        value: 17,
+        label: "Art"
+    },
+    {
+        value: 18,
+        label: "Celebrities"
+    },
+    {
+        value: 19,
+        label: "Animals"
+    },
+    {
+        value: 20,
+        label: "Vehicles"
+    },
+    {
+        value: 21,
+        label: "Entertainment: Comics"
+    },
+    {
+        value: 22,
+        label: "Science: Gadgets"
+    },
+    {
+        value: 23,
+        label: "Entertainment: Japanese Anime & Manga"
+    },
+    {
+        value: 24,
+        label: "Entertainment: Cartoon & Animations"
+    }
+]
+
+
+
--- frontend/src/components/Selections.js
+++ frontend/src/components/Selections.js
@@ -1,23 +1,72 @@
 import styled from "styled-components/macro"
-import DifficultySelector from "./DifficultySelector";
-import NumberSelector from "./NumberSelector";
-import CategorySelector from "./CategorySelector";
+import {useState} from "react";
+import useStyles from "./DropdownStyle";
+import {FormControl, MenuItem, Select} from "@material-ui/core";
+import {difficultyList} from "./DifficultyList";
+import {categoryList} from "./CategoryList";
+import {numberList} from "./NumberList";
+import {callQuestionList} from "../services/apiService";
+import NextButton from "./NextButton";
+
 
 export default function Selections() {
+
+    const style = useStyles();
+
+    const [category, setCategory] = useState("");
+    const handleChange = selection => setCategory(selection.target.value);
+
+
+    const [amount, setAmount] = useState("");
+    const handleChangeNumber = selection => setAmount(selection.target.value);
+
+
+    const [difficulty, setDifficulty] = useState("");
+    const handleChangeDifficulty = selection => setDifficulty(selection.target.value);
+
+
+    const triviaApiParametersDTO = {
+        "amount": amount,
+        "category": category,
+        "difficulty": difficulty
+    };
+
     return (
         <Wrapper>
             <section>
-                <header> Please select number of questions: </header>
-                <NumberSelector/>
+                <header> Please select number of questions:</header>
+                <FormControl className={style.formControl}>
+                    <Select className={style.Select} disableUnderline={true} onChange={handleChangeNumber} value={amount}>
+                        {numberList.map(number =>
+                            <MenuItem className={style.MenuItem} key={number.value}
+                                      value={number.value}> {number.label}</MenuItem>
+                        )}
+                    </Select>
+                </FormControl>
             </section>
             <section>
-                <header> Please select category: </header>
-                <CategorySelector/>
+                <header> Please select category:</header>
+                <FormControl className={style.formControl}>
+                    <Select className={style.Select} disableUnderline={true} onChange={handleChange} value={category}>
+                        {categoryList.map(category =>
+                            <MenuItem key={category.value} value={category.value}> {category.label}</MenuItem>
+                        )}
+                    </Select>
+                </FormControl>
             </section>
             <section>
-                <header> Please select level: </header>
-                <DifficultySelector/>
+                <header> Please select level:</header>
+                <FormControl className={style.formControl}>
+                    <Select className={style.Select} disableUnderline={true} onChange={handleChangeDifficulty} value={difficulty}>
+                        {difficultyList.map(difficulty =>
+                            <MenuItem key={difficulty.value} value={difficulty.value}> {difficulty.label}</MenuItem>
+                        )}
+                    </Select>
+                </FormControl>
             </section>
+            <footer>
+                <NextButton triviaApiParametersDTO={triviaApiParametersDTO} />
+            </footer>
         </Wrapper>
     )
 }
@@ -26,12 +75,18 @@ const Wrapper = styled.main`
   overflow-y: scroll;
   padding: 0 20px;
   display: grid;
-  grid-template-rows: auto auto auto;
+  grid-template-rows: 1fr 1fr 1fr auto;
   grid-gap: 10px;
 
   header {
     font-size: 1.2em;
     padding: 30px 10px 10px 0;
   }
+
+  footer {
+    display: flex;
+    justify-content: flex-end;
+  }
+
 
 `
