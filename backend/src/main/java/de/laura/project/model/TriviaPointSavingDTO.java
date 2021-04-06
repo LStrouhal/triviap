166cfc10925d33f75982e92c4b871d2b6d8f51fa
Laura Strouhal <laura_strouhal@hotmail.com>
2021-04-06 07:06:19 +0200

    added mongo db for user to save results in backend

--- backend/src/main/java/de/laura/project/model/TriviaPointSavingDTO.java
+++ backend/src/main/java/de/laura/project/model/TriviaPointSavingDTO.java
@@ -1,2 +1,19 @@
-package de.laura.project.model;public class TriviaPointSavingDTO {
+package de.laura.project.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class TriviaPointSavingDTO {
+
+    private int category;
+    private int amount;
+    private String difficulty;
+    private int points;
+
 }
+
