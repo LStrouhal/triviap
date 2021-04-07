5061cef08addaa8d610bc840ee53ccaa4dcd8deb
Laura Strouhal <laura_strouhal@hotmail.com>
2021-04-07 12:51:46 +0200

    added Logo

--- frontend/src/components/BackButton.js
+++ frontend/src/components/BackButton.js
@@ -1,20 +1,21 @@
-import styled from "styled-components/macro"
-import {BsArrowLeftShort} from "react-icons/bs"
+import styled from "styled-components/macro";
+import { BsArrowLeftShort } from "react-icons/bs";
 
 export default function BackButton() {
-    return (
-        <ArrowButton>
-            <BsArrowLeftShort/>
-        </ArrowButton>
-    )
+  return (
+    <ArrowButton>
+      <BsArrowLeftShort />
+    </ArrowButton>
+  );
 }
 
 const ArrowButton = styled.button`
-
   border: none;
   background: var(--beigeStandard);
   color: var(--greenStandard);
   padding: 10px 0 0 0;
   font-size: 40px;
   cursor: pointer;
-`
+  display: flex;
+  align-self: flex-start;
+`;
