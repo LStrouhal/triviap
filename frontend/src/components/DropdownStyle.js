3791c1a4aa39560320beb572b28746015523c5a1
Laura Strouhal <laura_strouhal@hotmail.com>
2021-04-11 10:15:34 +0200

    adjusted styling

--- frontend/src/components/DropdownStyle.js
+++ frontend/src/components/DropdownStyle.js
@@ -1,9 +1,14 @@
-import {makeStyles} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
 
-const useStyles = makeStyles(theme => ({
-    formControl: {minWidth: 200, background: "#f7f7f2", fontSize: "0.7em"},
-    MenuItem: {fontSize: "0.9em",},
-    Select: {marginLeft: 5,fontFamily: "Helvetica Neue"}
-}))
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    minWidth: 315,
+    background: "#f7f7f2",
+    fontSize: "0.7em",
+    borderRadius: "10px",
+  },
+  MenuItem: { fontSize: "0.9em" },
+  Select: { marginLeft: 7, fontFamily: "Helvetica Neue" },
+}));
 
 export default useStyles;
